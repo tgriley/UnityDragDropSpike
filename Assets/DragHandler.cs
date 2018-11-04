425152370ca7b07901643a716656d398f559e20a
Tom Riley <tgriley1@gmail.com>
2018-11-04 21:42:22 +0000

    Added project

---
+++ Assets/DragHandler.cs
@@ -0,0 +1,34 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [HideInInspector]
+    public GameObject TargetSlot;
+    
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        TargetSlot = transform.parent.gameObject;
+        transform.SetParent(transform.parent.parent.parent);
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.DOMove(TargetSlot.transform.position, 0.25f)
+            .SetEase(Ease.OutSine)
+            .OnComplete(() =>
+            {
+                transform.SetParent(TargetSlot.transform);
+                GetComponent<CanvasGroup>().blocksRaycasts = true;
+            });
+    }
+}
---
+++ Assets/DropHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropHandler : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        eventData.pointerDrag.GetComponent<DragHandler>().TargetSlot = gameObject;
+    }
+}
